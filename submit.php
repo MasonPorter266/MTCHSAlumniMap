ddc50295a30be2af41a9828c985e9a725dae84cb
Josh-Sparks <joshua.sparks@mtchs.org>
2018-05-07 08:05:01 -0600

    as
    aaa

--- submit.php
+++ submit.php
@@ -16,13 +16,13 @@
 	if(!$conn){
 		die("Connection failed:");
 	}else{
-		$sql = "SELECT DISTINCT Name, Email `MapLocation`";
+		$sql = "SELECT DISTINCT Name, Email FROM `Alumni`";
 		$result = $conn->query($sql);
 		
 	}
 	
-	$Name = $_GET['Name'];
 	$email = $_GET['email'];
+	$name = $_GET['name'];
 	$city = $_GET['city'];
 	$state = $_GET['state'];
 	$year = $_GET['year'];
@@ -35,25 +35,29 @@
 	$msg = "An error has occured,";
 	$empty ="";
 	
+	$isupdate = false;
 	while($row = $result->fetch_assoc()){
-		if($Name = $row['Name']){
-			$update = true;
+		if($name == $row['Name'] || $email == $row['Email']){
+			$isupdate = true;
 		}
 	}
 	if($fake == $empty){
-		if($Name != $empty && $email !=$empty && $city !=$empty && $state !=$empty && $year !=$empty && $currentJob !=$empty && $salary !=$empty && $isRelated !=$empty){
-			if($update){
+		if($name != $empty && $email !=$empty && $city !=$empty && $state !=$empty && $year !=$empty && $currentJob !=$empty && $salary !=$empty && $isRelated !=$empty){
+			if($isupdate == true){
 				echo "<script>alert('An alumni with similar information is already registered. This will be counted as an update</script>";
-				echo "<script> location.href = 'alumni.php'; </script>";
+				$msg2 = "" . $name . " Would like to update their information! Their email is " . $email . "\r\n City: " . $city . "\r\n" . " State: " . $state . "\r\n" . " Graduation Year: " . $year . "\r\n" . " Current Job: " . $currentJob . "\r\n" . " Current salary " . $salary . "\r\n" . " And is it related to the pathway?: " . $isRelated;
+				mail("joshua.sparks@mtchs.org","Alumni Submission",$msg2);
+				echo "<script>  location.replace(\"alumni.php\") </script>";
 			}
 			else{
-				echo "<script>alert('New Alumni Entry Submited')</script>" . "<p>" . $Name . $email . $city . $state . $year . $currentJob . $salary . $isRelated . "</p>";
-				echo "<script> location.href = 'alumni.php'; </script>";
+				echo "<script>alert('New Alumni Entry Submited')</script>";
+				$msg2 = "" . $name . " Has submitted a form! Their email is " . $email . "\r\n City: " . $city . "\r\n" . " State: " . $state . "\r\n" . " Graduation Year: " . $year . "\r\n" . " Current Job: " . $currentJob . "\r\n" . " Current salary: " . $salary . "\r\n" . " And is it related to the pathway?: " . $isRelated;
+				mail("joshua.sparks@mtchs.org","Alumni Submission",$msg2);
+				echo "<script>  location.replace(\"alumni.php\") </script>";
 			}
 		}
 		else{
-			echo "<script>alert('$msg all feilds must be entered in order to submit a new alumni form')</script>" . "<p>" . $Name . $email . $city . $state . $year . $currentJob . $salary . $isRelated . "</p>";
-			echo "<script> location.href = 'alumni.php'; </script>";
+			echo "<script>alert('$msg all feilds must be entered in order to submit a new alumni form');</script>";
 		}
 	}
 	else{
